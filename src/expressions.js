36a912c56bc81086c0fdbab0f373fa20ab168eb1
the-duck <markspurgeon96@hotmail.com>
2018-06-04 22:35:35 +0200

    added expressions list

---
+++ src/expressions.js
@@ -0,0 +1,14 @@
+var ScriptoExpressions = [
+  /* N°1 Priority Expressions */
+  "§A", // Act
+  "§S", // Scene
+  "§C", // Character
+  "§CA", // Character's Action or State
+  "§D", // Dialogue, or -logue
+  "§I", // Instructions or used for 'micro-scenes' (in capital letters)
+  "§P", // Paragraph - description of state or action
+  /* Generic Expressions that might be useful, and used for multiple purposes */
+  "§T", // Title
+  "§ST", // Subtitle
+  "§DATE", // Program-controlled date data, might be useful for user but not final draft
+]
